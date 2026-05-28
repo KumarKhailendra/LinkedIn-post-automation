5796e4ba2461769dd05fa1961b23eaa629c0cae6
KumarKhailendra <khailendrakumar417@gmail.com>
2026-05-28 21:14:24 +0530

    bug fixed or minnor update

--- linkedin_Scheduler/posts/tests.py
+++ linkedin_Scheduler/posts/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+from django.test import Client, TestCase
 
-# Create your tests here.
+
+class DashboardHostTests(TestCase):
+    def test_home_accepts_runserver_bind_host(self):
+        response = Client(HTTP_HOST='0.0.0.0:8000').get('/')
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_accepts_localhost(self):
+        response = Client(HTTP_HOST='localhost:8000').get('/')
+
+        self.assertEqual(response.status_code, 200)
